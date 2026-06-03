43aa1b3ab010d83e827784b79a7127534b0deb63
ElieLECAS <elielecas00@gmail.com>
2026-06-03 13:14:54 +0200

    feat: implement dbt pipeline for job offer market analysis with geographical and salary models

---
+++ 03_transform/dbt/models/marts/mart_offres_data_jobs.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='table'
+) }}
+
+with adzuna_offers as (
+    select
+        offer_id,
+        'Adzuna' as source_system,
+        job_title,
+        job_description,
+        created_at,
+        contract_type,
+        code_postal,
+        nom_commune,
+        numero_departement,
+        nom_departement,
+        nom_region,
+        nom_epci,
+        employer_name,
+        salary_min,
+        salary_max,
+        has_salary_info,
+        is_alternance
+    from {{ ref('int_adzuna_offres') }}
+)
+
+select * from adzuna_offers
